4217522621b90d72224b7b381ebee8fd44444392
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-15 16:15:43 -0600

    Added functionality for character counter, including a color change when values are negative.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function() {
+    const chars = $(this).val();
+
+    if (chars.length < 141) {
+    $(".counter").html(`${140 - chars.length}`);
+    } else {
+    $(".counter").html(`<a class="negativeValues">${140 - chars.length}</a>`);
+    }
+  });
+});
+
+
