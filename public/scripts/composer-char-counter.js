766b62e96429e3cdccf3da795ff588678c3c9753
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-06-15 21:36:57 -0600

    Finished the tweet template, added modular time stamp

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,6 +9,29 @@ $(document).ready(function() {
     $(".counter").html(`<a class="negativeValues">${140 - chars.length}</a>`);
     }
   });
+  // These two JQuery statements add shadow to tweets and hover property to icons
+  $("#tweet").hover(function(){
+    $(this).toggleClass("hoverOver");
+  });
+
+  //Change Icon colors on hover
+  $("i.fas.fa-flag").hover(function(){
+    $(this).toggleClass("iconColor");
+  })
+
+  $("i.fas.fa-retweet").hover(function(){
+    $(this).toggleClass("iconColor");
+  })
+
+  $("i.fas.fa-heart").hover(function(){
+    $(this).toggleClass("iconColor");
+  })
+});
+
+$(document).ready(function() {
+  const timeElement = $('time.timeago');
+  const newTime = timeago.format(timeElement.attr("datetime"));
+  timeElement.append(newTime);
 });
 
 
